32c267413ab24b2a606cf904777a8bd89cf12c47
david hurd <dhurd77@gmail.com>
2017-08-21 09:10:24 -0400

    added gameplay scene

---
+++ GameplayScene.swift
@@ -0,0 +1,19 @@
+//
+//  GameplayScene.swift
+//  Jack the Giant
+//
+//  Created by David Hurd on 8/21/17.
+//  Copyright © 2017 Imagitale Studios. All rights reserved.
+//
+
+import SpriteKit
+
+class GameplayScene : SKScene {
+    override func didMove(to view: SKView) {
+        print("Game loaded")
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        
+    }
+}
