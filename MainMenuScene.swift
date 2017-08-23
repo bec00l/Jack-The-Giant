0e570d7ed5ca22c0079a7dd75bf66df4d7936dff
david hurd <dhurd77@gmail.com>
2017-08-22 20:46:25 -0400

    added main menu

---
+++ MainMenuScene.swift
@@ -0,0 +1,15 @@
+//
+//  MainMenuScene.swift
+//  Jack the Giant
+//
+//  Created by David Hurd on 8/22/17.
+//  Copyright © 2017 Imagitale Studios. All rights reserved.
+//
+
+import SpriteKit
+
+class MainMenuScene : SKScene {
+    override func didMove(to view: SKView) {
+        
+    }
+}
